77e5d9cb53e0167d79fbbaba753fffbafd44180f
doosemavis <moosedavis2011@gmail.com>
2020-09-23 09:57:37 -0500

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,22 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :status
+  attr_reader :amount, :sender, :receiver
+
+  def initialize(sender, reciever, amount)
+    @status = "pending"
+    @amount = amount  
+    @receiver = receiver
+    @sender = sender
+  end 
+
+  def valid?
+  end 
+
+  def execute_transaction
+  end 
+
+  def reverse_transfer
+  end 
+
 end
