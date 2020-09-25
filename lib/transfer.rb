393bf153397c21f89b109f190a4209b42bc71682
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-25 16:06:30 +0000

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,11 @@ class Transfer
     end
   end
   
-
+  def reverse_transfer
+    if status == "complete"
+      @sender.deposit(@amount)
+      @receiver.deposit(@amount * -1)
+      @status = "reversed"
     end
   end
 
